6a9c8110f6cf5b922bc550fc07230137a1f3e4da
Ricky Li <wkliwk@gmail.com>
2017-08-01 01:58:44 +0800

    update config

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-// Copyright 2017, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+'use strict'
 
-'use strict';
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const express = require('express');
+const app = express()
 
-const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 // [START hello_world]
 // Say hello!
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!');
-});
+  console.log(req)
+  // console.log(res)
+  const body = req.body ? req.body : null
+  const header = req.headers ? req.headers : null
+  const url = req.url ? `${header.host}${req.url}` : null
+  const method = req.method ? req.method : null
+  const queryStr = req.query? req.query : null
+
+  // console.log(res.headers)
+  const resJson = {method, url, queryStr, body, header}
+  res.status(200).json(resJson)
+})
+
 // [END hello_world]
 
 if (module === require.main) {
   // [START server]
   // Start the server
   const server = app.listen(process.env.PORT || 8081, () => {
-    const port = server.address().port;
-    console.log(`App listening on port ${port}`);
-  });
+    const port = server.address().port
+    console.log(`App listening on port ${port}`)
+  })
   // [END server]
 }
 
-module.exports = app;
+module.exports = app
